fe7f8d930195dc2fa52430810d332548ee9f15df
bharadwaj-it <mattabharadwaj2@gmail.com>
2026-03-26 21:00:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("form").addEventListener("submit",function(event)
+{
+    event.preventDefault();
+    alert("Message submitted successfully!");
+});
+console.log("Portfolio working");
+console.log("Detected now");
